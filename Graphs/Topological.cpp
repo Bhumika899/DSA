b8f79a732b6283e7a6905c05f464dede242ec29a
bhumika_03 <bhumikasabharwal0@gmail.com>
2026-08-07 08:47:51 +0530

    TOPOLOGICAL SORT

---
+++ Graphs/Topological.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    private:
+    void dfs(int node,int vis[],stack<int>&st,vector<vector<int>>& adj){
+        vis[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]) dfs(it,vis,st,adj);
+        }
+        st.push(node);
+    }
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+         vector<vector<int>> adj(V);
+        for(auto edge : edges) {
+            // Assuming directed graph from edge[0] to edge[1]
+            adj[edge[0]].push_back(edge[1]); 
+        }
+        int vis[V]={0};
+        stack<int>st;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,vis,st,adj);
+            }
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
