73cd29b70ee5adaf84f3c9722ee1ee30bad0a2fd
bhumika_03 <bhumikasabharwal0@gmail.com>
2026-08-16 21:08:31 +0530

    ARRAY TO LINKED LIST

---
+++ LinkedList/ArrToLin.cpp
@@ -0,0 +1,29 @@
+/*
+// Representation of a node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* arrayToList(vector<int>& arr) {
+        // code here
+        if(arr.empty()) return NULL;
+        Node* head=new Node(arr[0]);
+        Node* curr=head;
+        for(int i=1;i<arr.size();i++){
+            curr->next=new Node(arr[i]);
+            curr=curr->next;
+            
+        }
+        return head;
+    }
+};
