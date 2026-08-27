89d1f61586f29140ca8b8b195ce0830b777adad5
bhumika_03 <bhumikasabharwal0@gmail.com>
2026-08-27 10:50:36 +0530

    Delete All Occurrences in DLL

---
+++ LinkedList/DeleteAllDLL.cpp
@@ -0,0 +1,38 @@
+/* a Node of the doubly linked list
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+        prev = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* deleteAllOccurOfX(Node* head, int x) {
+        // code here
+        Node *temp=head;
+        while(temp!=NULL){
+            if(temp->data==x){
+                if(temp==head){
+                    head=temp->next;
+                }
+                Node* nextNode=temp->next;
+                Node* prevNode=temp->prev;
+                if(nextNode!=NULL) nextNode->prev=prevNode;
+                if(prevNode!=NULL) prevNode->next=nextNode;
+                free(temp);
+                temp=nextNode;
+                }else{
+                    temp=temp->next;
+                }
+                
+        }
+        return head;
+    }
+};
